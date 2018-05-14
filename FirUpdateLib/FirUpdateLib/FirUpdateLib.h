392c5245eb67d0a5bf68dcde47f53406218f9456
Jin@Jian <kimtrickster@gmail.com>
2018-05-14 19:09:37 +0800

    Update FirUpdateLib.h

--- FirUpdateLib/FirUpdateLib/FirUpdateLib.h
+++ FirUpdateLib/FirUpdateLib/FirUpdateLib.h
@@ -10,9 +10,9 @@
 
 
 #define FIR_SERVER      @"http://api.fir.im/apps/latest/"
-#define FIR_UPDATEURL   @"https://fir.im/udapp"
-#define FIR_TOKEN       @"f862d8abfe923cc8bacbf90f8521a4c3"
-#define FIR_APPID       @"5aea7c87959d6953cada53bc"
+#define FIR_UPDATEURL   @""
+#define FIR_TOKEN       @""
+#define FIR_APPID       @""
 #define ALERT_MESSAGE   @"监测到新版本，请更新!"
 
 @interface FirUpdateLib : NSObject
